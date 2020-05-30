261f3d9a51b2bf3c2460eb164a69038464b424b9
Souvik Biswas <sbis1999@gmail.com>
2020-05-30 09:47:10 +0530

    Completed UI for Build Config

--- lib/build_config_screen.dart
+++ lib/build_config_screen.dart
@@ -41,210 +41,235 @@ class _BuildConfigScreenState extends State<BuildConfigScreen> {
     var width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            color: Colors.orange[800],
-            height: height * 0.20,
-            width: double.maxFinite,
-            child: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 20.0),
-                child: Stack(
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: BackButton(
-                        color: Colors.white,
-                        onPressed: () => Navigator.of(context).pop(),
+      body: Container(
+        height: height,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              color: Colors.orange[800],
+            ),
+            Container(
+              color: Colors.orange[800],
+              height: height * 0.20,
+              width: double.maxFinite,
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Stack(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: BackButton(
+                          color: Colors.white,
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
                       ),
+                      Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Codemagic Connect'.toUpperCase(),
+                              style: GoogleFonts.rubik(
+                                fontSize: 15,
+                                color: Colors.orange,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Build Config'.toUpperCase(),
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: height * 0.15,
+              height: height,
+              width: width,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Container(
+                    height: height * 1.1,
+                    // width: double.maxFinite,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 20.0, // soften the shadow
+                          spreadRadius: 5.0, //extend the shadow
+                          offset: Offset(
+                            10.0, // Move to right 10  horizontally
+                            10.0, // Move to bottom 10 Vertically
+                          ),
+                        )
+                      ],
                     ),
-                    Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(
-                            'Codemagic Connect'.toUpperCase(),
-                            style: GoogleFonts.rubik(
-                              fontSize: 15,
-                              color: Colors.orange,
-                              fontWeight: FontWeight.bold,
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Row(),
+                              Text(
+                                widget.application.appName,
+                                style: GoogleFonts.varelaRound(
+                                  fontSize: 20,
+                                ),
+                              ),
+                              SizedBox(height: 8.0),
+                              // TODO: URL Launcher for GitHub Link
+                              Text(
+                                widget.application.repository.htmlUrl
+                                    .substring(8),
+                                style: GoogleFonts.robotoCondensed(
+                                  fontSize: 16,
+                                  color: Colors.black45,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Divider(
+                              thickness: 2,
                             ),
                           ),
-                          Text(
-                            'Build Config'.toUpperCase(),
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
+                          Column(
+                            children: <Widget>[
+                              _dropDownTile(
+                                'Select Branch',
+                                _branches,
+                                _selectedBranch,
+                                width,
+                                (String value) {
+                                  setState(() {
+                                    _selectedBranch = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 15),
+                          _dropDownTile(
+                            'Select Workflow',
+                            _workflowNames,
+                            _selectedWorkflowName,
+                            width,
+                            (String value) {
+                              setState(() {
+                                _selectedWorkflowName = value;
+                              });
+                            },
+                          ),
+                          Center(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 30.0, bottom: 5.0),
+                              child: Text(
+                                'ENVIRONMENT VARIBLES',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
                             ),
+                          ),
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.only(bottom: 20.0),
+                              child: Text(
+                                '(optional)',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: <Widget>[
+                              _inputTile(
+                                text: 'Flutter',
+                                hint: 'Enter the Flutter verison',
+                              ),
+                              _inputTile(
+                                text: 'Xcode',
+                                hint: 'Enter the Xcode verison',
+                              ),
+                              _inputTile(
+                                text: 'CocoaPods',
+                                hint: 'Enter the CocoaPods verison',
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _inputTile({String text, String hint}) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, bottom: 10.0),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 8.0),
+            child: TextFormField(
+              initialValue: '',
+              cursorColor: Colors.black,
+              onChanged: (value) {
+                // setState(() {
+                //   _map[entryList[index].key] = value;
+                // });
+              },
+              decoration: new InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.grey, width: 1),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  borderSide: BorderSide(color: Colors.blue, width: 3),
+                ),
+                contentPadding:
+                    EdgeInsets.only(left: 15, bottom: 25, top: 25, right: 15),
+                hintText: hint,
               ),
             ),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: height * 0.85,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.white,
-                ),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    blurRadius: 20.0, // soften the shadow
-                    spreadRadius: 5.0, //extend the shadow
-                    offset: Offset(
-                      10.0, // Move to right 10  horizontally
-                      10.0, // Move to bottom 10 Vertically
-                    ),
-                  )
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Row(),
-                        Text(
-                          widget.application.appName,
-                          style: GoogleFonts.varelaRound(
-                            fontSize: 20,
-                          ),
-                        ),
-                        SizedBox(height: 8.0),
-                        // TODO: URL Launcher for GitHub Link
-                        Text(
-                          widget.application.repository.htmlUrl.substring(8),
-                          style: GoogleFonts.robotoCondensed(
-                            fontSize: 16,
-                            color: Colors.black45,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Divider(
-                        thickness: 2,
-                      ),
-                    ),
-                    _dropDownTile(
-                      'Select Branch',
-                      _branches,
-                      _selectedBranch,
-                      width,
-                      (String value) {
-                        setState(() {
-                          _selectedBranch = value;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 15),
-                    _dropDownTile(
-                      'Select Workflow',
-                      _workflowNames,
-                      _selectedWorkflowName,
-                      width,
-                      (String value) {
-                        setState(() {
-                          _selectedWorkflowName = value;
-                        });
-                      },
-                    ),
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 30.0, bottom: 5.0),
-                        child: Text(
-                          'ENVIRONMENT VARIBLES',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 20.0),
-                        child: Text(
-                          '(optional)',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ),
-                    ),
-                    // Padding(
-                    //   padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                    //   child: Column(
-                    //     crossAxisAlignment: CrossAxisAlignment.start,
-                    //     children: <Widget>[
-                    //       Padding(
-                    //         padding:
-                    //             const EdgeInsets.only(left: 15.0, bottom: 10.0),
-                    //         child: Text(
-                    //           'Select Branch',
-                    //           style: TextStyle(fontSize: 18),
-                    //         ),
-                    //       ),
-                    //       Center(
-                    //         child: Container(
-                    //           width: width * 0.9,
-                    //           decoration: BoxDecoration(
-                    //             color: Colors.transparent,
-                    //             border:
-                    //                 Border.all(color: Colors.orange, width: 3),
-                    //             borderRadius: BorderRadius.all(
-                    //               Radius.circular(20.0),
-                    //             ),
-                    //           ),
-                    //           child: Padding(
-                    //             padding: const EdgeInsets.fromLTRB(
-                    //               15.0,
-                    //               8.0,
-                    //               15.0,
-                    //               8.0,
-                    //             ),
-                    //             child: DropdownButton(
-                    //               isExpanded: true,
-                    //                 underline: Container(),
-                    //                 icon: Icon(Icons.arrow_drop_down),
-                    //                 items: _branches.map((String value) {
-                    //                   return new DropdownMenuItem<String>(
-                    //                     value: value,
-                    //                     child: new Text(value),
-                    //                   );
-                    //                 }).toList(),
-                    //                 value: _selectedBranch,
-                    //                 onChanged: (value) {
-                    //                   setState(() {
-                    //                     _selectedBranch = value;
-                    //                   });
-                    //                 }),
-                    //           ),
-                    //         ),
-                    //       )
-                    //     ],
-                    //   ),
-                    // ),
-                  ],
-                ),
-              ),
-            ),
-          )
         ],
       ),
     );
@@ -273,9 +298,12 @@ class _BuildConfigScreenState extends State<BuildConfigScreen> {
             child: Container(
               width: width * 0.9,
               decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  border: Border.all(color: Colors.orange, width: 3),
-                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                color: Colors.transparent,
+                border: Border.all(color: Colors.orange, width: 3),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
+              ),
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(15.0, 8.0, 15.0, 8.0),
                 child: DropdownButton(
